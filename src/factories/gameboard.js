6deb3d342206fb5f8c25733a93c543132d54ec48
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-10 13:07:24 +0200

    Add placing of ships

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,15 +1,59 @@
-const size = 10;
+import { SHIP_TYPES } from "../helpers/helpers";
 
-export default class Gameboard {
-    constructor(board) {
-        this.board = board || []; //for custom board sizes
-        this.missedShots = [];
-        if (!this.board.length) this.initialize();
+const Gameboard = () => {
+    let board = Array(10).fill(null).map(() => Array(10).fill(null));
+    const getBoard = () => board;
+
+    let placedShips = [];
+    const areAllShipsPlaced = () => placedShips.length === SHIP_TYPES.length;
+
+    const _adjustCoords = (y0, x0, i, direction) => {
+        let x = x0 +i;
+        let y = y0;
+        if (direction === "vertical") {
+            x = x0;
+            y = y0+i;
+        }
+        return[y, x];
     }
 
-    initialize() {
-        for (let i = 0; i < 100; i++) {
-            this.board.push({ hasShip: false, isShot: false});
+    const checkValid = (length, direction, y0, x0) => {
+        const cells = [];
+        for (let i = 0; i < length; i++) {
+            const [y, x] = _adjustCoords(y0, x0, i, direction);
+
+            if(y < 10 && x < 10) {
+                cells.push(board[y][x]);
+            } else {
+                return false;
+            }
+        }
+        return cells.every((cell) => cell === null);
+    }
+
+    const placeShip = (ship, y0, x0) => {
+        const direction = ship.getDirection();
+        const valid = checkValid(ship.length, direction, y0, x0);
+        if (valid) {
+            for (let i = 0; i < ship.length; i++) {
+                const [y, x] = _adjustCoords(y0, x0, i, direction);
+                //place ship with index
+                board[y][x] = { ship, index: i}
+            }
+
+            placedShips.push(ship);
+            return valid;
+        } else {
+            return valid;
         }
     }
+
+    return {
+        getBoard,
+        areAllShipsPlaced,
+        placeShip,
+
+    }
 }
+
+export default Gameboard;
--- src/factories/tests/gameboard.test.js
+++ src/factories/tests/gameboard.test.js
@@ -2,16 +2,61 @@ import Ship from "../ship";
 import Gameboard from "../gameboard";
 
 describe("Gameboard", () => {
-    let testBoard ;
-    beforeEach(() => {
-        testBoard = new Gameboard();
+    describe("board", () => {
+        const gameboard = Gameboard();
+        test("empty board", () => {
+            const actual = gameboard.getBoard().every((square) => square === null);
+            const expected = false;
+            expect(actual).toBe(expected);
+        })
+        test("board row", () => {
+            const actual = gameboard.getBoard().length;
+            const expected = 10;
+            expect(actual).toBe(expected);
+        })
+        test("board column", () => {
+            const actual = gameboard.getBoard()[0].length;
+            const expected = 10;
+            expect(actual).toBe(expected);
+        })
     })
 
-    test("creates a gameboard with right amound of cells", () => {
-        const arr = [];
-        for (let i = 0; i < 100; i++) {
-            arr.push({ hasShip: false, isShot: false});
-        }
-        expect(testBoard.board).toEqual(arr);
+    describe("place horizontal ship", () => {
+        const gameboard = Gameboard();
+        const ship = new Ship("cruiser");
+        gameboard.placeShip(ship, 3, 2);
+
+        test("placed ship is at starter coord with index 0", () => {
+            const actual = gameboard.getBoard()[3][2];
+            expect(actual).toEqual({ ship, index: 0})
+        })
+        test("placed ship is at starter coord with index 1", () => {
+            const actual = gameboard.getBoard()[3][3];
+            expect(actual).toEqual({ ship, index: 1})
+        })
+        test("placed ship is at starter coord with index 2", () => {
+            const actual = gameboard.getBoard()[3][4];
+            expect(actual).toEqual({ ship, index: 2})
+        })
+    })
+    
+    describe("place vertical ship", () => {
+        const gameboard = Gameboard();
+        const ship = new Ship("submarine");
+        ship.changeDirection();
+        gameboard.placeShip(ship, 3, 2);
+
+        test("placed ship at starter coords with index 0", () => {
+            const actual = gameboard.getBoard()[3][2];
+            expect(actual).toEqual({ship, index: 0});
+        })
+        test("placed ship at starter coords with index 1", () => {
+            const actual = gameboard.getBoard()[4][2];
+            expect(actual).toEqual({ship, index: 1});
+        })
+        test("placed ship at starter coords with index 2", () => {
+            const actual = gameboard.getBoard()[5][2];
+            expect(actual).toEqual({ship, index: 2});
+        })
     })
 }) 
