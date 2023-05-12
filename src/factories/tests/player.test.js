9f3264e6cac9c8fcea242d5d438e58028f9d9578
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-12 12:01:44 +0200

    Update player

--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -1,18 +1,21 @@
 import Player from "../player";
 import Gameboard from "../gameboard";
+import Ship from "../ship";
 
 const player = new Player("human");
 const enemy = new Player("computer");
 const playerBoard = Gameboard();
 const enemyBoard = Gameboard();
+const submarine = new Ship("submarine");
+playerBoard.placeShip(submarine, 2, 0);
 
 describe("Types", () => {
-    test("human type", () => {
-        const actual = player.getType();
+    test("name: human", () => {
+        const actual = player.getName();
         expect(actual).toBe("human");
     })
-    test("computer type", () => {
-        const actual = enemy.getType();
+    test("name: computer", () => {
+        const actual = enemy.getName();
         expect(actual).toBe("computer");
     })
 })
@@ -22,6 +25,14 @@ describe("Player attack", () => {
         player.attack(2, 3, enemyBoard);
         const actual = enemyBoard.getBoard()[2][3];
         expect(actual).toBe("miss");
+    })
+})
+
+describe("Auto attack", () => {
+    test("enemy auto attack player board", () => {
+        enemy.autoAttack(playerBoard); 
+        const actual = playerBoard.getBoard().flat().every((cell) => cell === null);
+        expect(actual).toBe(false);
     })
 })
 
