fcecfa382396ef8a14bf9a7825a92bda3a8f5f71
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-16 13:08:20 +0200

    Add attack funcs, test and placement logic

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -5,16 +5,50 @@ const Gameboard = () => {
         board.push({hasShip: false, isShot: false});
     }                                                 
 
-    let placedShips = [];
+    const locationArray = (location, ship, axis) => {
+        const locationArray = [];
+        for (let i = 0; i < ship.length; i++) {
+            axis === "x" ? locationArray.push(location + i) : locationArray.push(location + i * 10);
+        }
+        return locationArray;
+    }
 
-    const placeShip = (ship, y0, x0) => {
+    const receiveAttack = (location) => {
+        board[location].isShot = true;
+    }
 
+    const checkIfAttackHit = (location) => {
+        return board[location].hasShip;
+    }
+
+    const checkCollisions = (locationArray) => {
+        //on x axis, stops going over boundaries
+        const collisions = [9, 19, 29, 39, 49, 59, 69, 79, 89];
+        if (locationArray.some((loc) => board[loc])) {
+            //check for boundaries
+            return false;
+        } else if (locationArray.some((loc) => board[loc].hasShip)) {
+            //checks for other Ships
+            return false;
+        } else if (
+            collisions.some((num) => {
+                return [num, num +1].every((combination) => locationArray.includes(combination));
+            })
+        ) {
+            return false
+        } else {
+            return true;
+        }
     }
 
     const getBoard = () => board;
 
     return {
-        getBoard,        
+        getBoard,
+        locationArray,
+        checkCollisions,
+        receiveAttack,
+        checkIfAttackHit
     }
 }
 
--- src/factories/tests/gameboard.test.js
+++ src/factories/tests/gameboard.test.js
@@ -2,14 +2,31 @@ import Gameboard from "../gameboard";
 
 describe("test gameboard creation", () => {
     describe("board", () => {
-        let testBoard = Gameboard().getBoard();
-            
+        let testBoard;
+        beforeEach(() => {
+            testBoard = Gameboard();
+        })
+
         test("empty board", () => {
             const arr = [];
             for (let i = 0; i < 100; i++) {
                 arr.push({hasShip: false, isShot: false});
             } 
-        expect(testBoard).toEqual(arr);
+        expect(testBoard.getBoard()).toEqual(arr);
+        })
+        test("update cell if shot", () => {
+            testBoard.receiveAttack(33);
+            expect(testBoard.getBoard()[33].isShot).toBe(true);
+        })
+        test("doesnt produce a fake result", () => {
+            expect(testBoard.getBoard()[45].isShot).toBe(false);
+        })
+        test("reports if shot missed", () => {
+            expect(testBoard.checkIfAttackHit(25)).toBe(false);
+        })
+        test("confirms a hit", () => {
+            testBoard.getBoard()[33].hasShip = true;
+            expect(testBoard.checkIfAttackHit(33)).toBe(true);
         })
     })
 })
