04fe28903a2bcfb690f75dfc52bda1e8820ce200
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-16 15:12:57 +0200

    Add testing

--- src/factories/tests/gameboard.test.js
+++ src/factories/tests/gameboard.test.js
@@ -1,4 +1,5 @@
 import Gameboard from "../gameboard";
+import Ship from "../ship";
 
 describe("test gameboard creation", () => {
     describe("board", () => {
@@ -27,6 +28,19 @@ describe("test gameboard creation", () => {
         test("confirms a hit", () => {
             testBoard.getBoard()[33].hasShip = true;
             expect(testBoard.checkIfAttackHit(33)).toBe(true);
+        })
+        test("rejects ship that collides with another ship", () => {
+            testBoard.getBoard()[12].hasShip = "carrier";
+            expect(testBoard.checkCollisions([2, 12, 22, 32])).toBe(false);
+        })
+        test("rejects ship that passes through map edge on x axis", () => {
+            expect(testBoard.checkCollisions([8, 9, 10, 11, 12])).toBe(false);
+        })
+        test("rejects ship that passes through map edge on y axis", () => {
+            expect(testBoard.checkCollisions([77, 87, 97, 107])).toBe(false);
+        })
+        test("returns location array of a ship", () => {
+            expect(testBoard.locationArray(23, Ship("destroyer"), "y")).toEqual([23, 33])
         })
     })
 })
