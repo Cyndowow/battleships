55a9f56c6a1eaeec0602829255176343078a7a9d
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-22 11:52:54 +0200

    Add automatic ship creation

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,15 +1,40 @@
 import Gameboard from "./gameboard";
+import Ship from "./ship";
 
 export default class Player {
     constructor(name) {
         this.name = name;
         this.ships = [];
         this.gameBoard = new Gameboard();
+        this.turn = false;
     }
 
-    fireShot(location, gameboard) {
-        if (gameboard.opponentBoard()[location] === "empty") {
-            gameboard.receiveAttack(location);
+    fireShot(player, pos1, pos2) {
+        return player.gameBoard.receiveAttack(pos1, pos2);
+    }
+
+    isTurn(enemy) {
+        this.turn = true;
+        enemy.turn = false;
+    }
+
+    randomPos() {
+        let pos1 = Math.floor(Math.random() * 10);
+        let pos2 = Math.floor(Math.random() * 10);
+        return [pos1, pos2];
+    }
+
+    createFleet() {
+        const shipLengthArray = [2, 3, 3, 4, 5];
+        for (const length of shipLengthArray) {
+            this.ships.push(new Ship(length));
         }
     }
+
+    /*placeShipsRandomly() {
+        this.createFleet();
+        this.ships.forEach((ship) => {
+            this.gameBoard.randomPos
+        })
+    }*/
 }
--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -1,20 +1,40 @@
 import Gameboard from "../gameboard";
 import Player from "../player";
+import Ship from "../ship";
+
+
+test("player automatically has gameboard", () => {
+    let player = new Player("human");
+    let gameboard = new Gameboard();
+    expect(JSON.stringify(player.gameBoard)).toEqual(JSON.stringify(gameboard)); //had to stringify because of serialization issue
+})
 
 describe("player test", () => {
     let player;
-    let playerBoard;
-    let oppBoard;
+    let opp;
     beforeEach(() => {
         player = new Player("human");
-        playerBoard = new Gameboard();
-        oppBoard = new Gameboard();
+        opp = new Player("opp");
     })
     test("create a player", () => {
         expect(player.name).toBe("human");
     })
     test("fire a shot at the opponents board", () => {
-        player.fireShot(25, oppBoard);
-        expect(oppBoard.board[25].isShot).toBe(true);
+        player.fireShot(opp, 0, 0);
+        expect(opp.gameBoard.board[0][0]).toBe("miss");
+    })
+    test("swap turn", () => {
+        player.isTurn(opp);
+        expect(player.turn).toBe(true);
+    })
+    test("swap turn back", () => {
+        player.isTurn(opp);
+        opp.isTurn(player);
+        expect(player.turn).toBe(false);
+    })
+    test("fleet creation", () => {
+        player.createFleet();
+        console.log(player.ships);
+        expect(player.ships).not.toBe(null);
     })
 })
