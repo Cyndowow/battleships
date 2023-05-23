604c0692b4fc37847bf506aee067d00efedae62c
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-05-23 12:04:47 +0200

    Add basic render function

--- src/DOM/game.js
+++ src/DOM/game.js
@@ -1,4 +1,5 @@
 import Player from "../factories/player";
+import { renderBoards} from "./renderFunction";
 
 let p1, p2;
 
@@ -6,8 +7,10 @@ function initGame() {
     p1 = new Player("You");
     p2 = new Player("Your enemy");
 
-    p1.isTurn(p2);
-    p2.createFleet();
+    //p1.isTurn(p2);
+    //p2.placeRandomFleet();
+
+    renderBoards(p1, p2);
 }
 
 export {initGame, p1, p2};
--- src/DOM/renderFunction.js
+++ src/DOM/renderFunction.js
@@ -1,12 +1,44 @@
 import { initGame, p1, p2} from "./game"
 
+
 function renderBoards(p1, p2) {
-    for (let i = 0; i < 100; i++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell-p1");
-        cell.setAttribute("id", `p1-index${i}`);
-        document.getElementById("playerBoard").appendChild(cell);
+    for(let i = 0; i < 10; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row-p1");
+        row.setAttribute("id", `p1-row${i}`);
+        document.getElementById("playerBoard").appendChild(row);
+        
+        p1.gameBoard.board[i].forEach((e, j) => {
+            let cell = document.createElement("div");
+            cell.classList.add("cell-p1");
+            cell.setAttribute("id", `p1-row${i}-cell${j}`);
+            row.appendChild(cell);
+
+            //event listener for clicks on p2 board
+            cell.addEventListener("click", (e) => {
+                if(!p1.getTurn() || !p1.gameBoard.getStartAllowed()) return;
+                renderAttackP1(e, i, j, p1, p2);
+            });
+        });
     }
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row-p2");
+        row.setAttribute("id", `p2-row${i}`);
+        document.getElementById("enemyBoard").appendChild(row);
+        
+        p1.gameBoard.board[i].forEach((e, j) => {
+            let cell = document.createElement("div");
+            cell.classList.add("cell-p2");
+            cell.setAttribute("id", `p2-row${i}-cell${j}`);
+            row.appendChild(cell);
+        })
+    }
+}
+
+function resetBoards() {
+    document.querySelector(".board-buttons").innerHTML = "";
+    document.querySelector(".ships").innerHTML = "";
 }
 
 export {
